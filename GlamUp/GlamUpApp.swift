418ceb96e6c13b971df74447e56f837cc9e9d6f5
Melissa Barbie <melisapaksoy@hotmail.com>
2026-02-06 15:19:11 -0500

    Initial Commit

---
+++ GlamUp/GlamUpApp.swift
@@ -0,0 +1,17 @@
+//
+//  GlamUpApp.swift
+//  GlamUp
+//
+//  Created by Melissa Barbie on 2026-02-06.
+//
+
+import SwiftUI
+
+@main
+struct GlamUpApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
